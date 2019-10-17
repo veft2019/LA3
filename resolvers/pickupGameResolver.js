2b228d0e8a190d787693df7275f9e30c4f5fa118
Guðrún Margrét Ívansdóttir <gudruni17@ru.is>
2019-10-17 16:46:17 +0000

    working on PickUpGameResolver

--- resolvers/pickupGameResolver.js
+++ resolvers/pickupGameResolver.js
@@ -1,3 +1,35 @@
 /*
 Should resolve a subset of the GraphQL schema for the pickup game
  */
+
+ const db = require('../data/db');
+
+
+
+ module.exports = {
+     queries: {
+         allPickupGames: () => db.PickupGame
+     },
+
+     mutations: {
+       createNewPickUpGame: (parent, args) => {
+            const newPickUpGame = {
+   
+               // input 
+               // start: Moment!
+               // end: Moment!
+               // basketballFieldId: String!
+               // hostId: String!
+   
+            }
+        },
+
+        updatePickUpGames: (parent, args) => {
+
+        },
+        deletePickUpGames: (parent, args) => {
+   
+        }
+     }
+
+ }
