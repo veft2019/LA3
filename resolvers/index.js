2ee54aaa1d22f3307acb15141cd36eb216090f39
Guðrún Margrét Ívansdóttir <gudruni17@ru.is>
2019-10-17 18:46:25 +0000

    PickUpGame Resolver (all, create and delete), ugly

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,11 +1,14 @@
 const playerResolver = require('./playerResolver');
+const pickupGameResolver = require('./pickupGameResolver');
 
 module.exports = {
     Query: {
+        ...pickupGameResolver.queries,
         ...playerResolver.queries
     },
     Mutation: {
+        ...pickupGameResolver.mutations,
         ...playerResolver.mutations
-    }
-    //types
+    },
+    ...pickupGameResolver.types
 };
