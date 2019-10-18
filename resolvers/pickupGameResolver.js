e44efd47f222804884949db1ba0b19cd5e49664a
HjorturJ <hjorturjohann94@gmail.com>
2019-10-18 15:27:05 +0000

    Get pickup games works with registered players now

--- resolvers/pickupGameResolver.js
+++ resolvers/pickupGameResolver.js
@@ -24,19 +24,45 @@
             return results;
         },
         removePickupGame: async (parent, args, { db }) => {
-            const gameToRemove = await db.PickupGame.findOneAndUpdate(args.id, {deleted: true}, {new: true })
+            const gameToRemove = await db.PickupGame.findOneAndUpdate(args.id, { deleted: true }, {new: true })
             return true;
         },
         addPlayerToPickupGame: async (parent, args, { db }) => {
             const playerId = await db.Player.findById(args.input.playerId); 
-            const result = await db.PickupGame.findOneAndUpdate(args.input.pickupGameId, {$push: {registerdPlayers: playerId}}, {new: true} )
+            const result = await db.PickupGame.findOneAndUpdate(args.input.pickupGameId, { $push: {registeredPlayers: args.input.playerId} }, {new: true} )
             return result;
         }
      },
      types: {
         PickupGame: {
-            location: async (parent, args, { db }) => await db.PickupGame.findById(parent.basketballFieldId),
+            location: async (parent, args, { db }) => await basketballFields.fieldById(parent.basketballFieldId),
             host: async (parent, args, { db }) => await db.Player.findById(parent.hostId),
+            registeredPlayers: async (parent, args, { db }) => {
+                const final = [];
+                const players = await db.Player.find({});
+                /*
+                console.log("Players!: ");
+                console.log(players);
+                console.log("Players done");
+                console.log("Parent!: ");
+                console.log(parent.registeredPlayers);
+                console.log("Parent done");
+                */
+                parent.registeredPlayers.forEach(pId => {
+                    players.forEach( p => {
+                        console.log(pId);
+                        console.log(p);
+                        if(pId == p.id) {
+                            final.push(p);
+                        }
+                    })
+                });
+                /*
+                console.log("Res: ");
+                console.log(final);
+                */
+               return final;
+            }
         }
      }
     
