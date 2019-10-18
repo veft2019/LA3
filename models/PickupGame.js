0e09cd6591a2a6a50e961822fe704c2e55dc1693
HjorturJ <hjorturjohann94@gmail.com>
2019-10-18 14:35:03 +0000

    Pickup game resolver create working now

--- models/PickupGame.js
+++ models/PickupGame.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 module.exports = new Schema({
     start: { type: String, required: true },
     end: { type: String, required: true },
-    location: { type: Schema.Types, ref: 'BasketballField', required: true },
+    basketballFieldId: { type: String, required: true },
     registeredPlayers: [{ type: String, required: true }],
-    host: { type: Schema.Types, ref: 'Player', required: true }
+    hostId: { type: Schema.Types.ObjectId, ref: 'Player', required: true }
 });
--- resolvers/pickupGameResolver.js
+++ resolvers/pickupGameResolver.js
@@ -1,32 +1,39 @@
- const db = require('../data/db');
+ //const db = require('../data/db');
  const errors = require('../errors'); 
  const basketballFields = require('../services/basketballFieldService');
 
 
  module.exports = {
      queries: {
-         allPickupGames: async () => await db.PickupGame.find({}),
-         pickupGame: async (parent, args) => await db.PickupGame.findById(args.id)
+         allPickupGames: async (parent, args, { db }) => await db.PickupGame.find({}),
+         pickupGame: async (parent, args, { db }) => await db.PickupGame.findById(args.id)
      },
      mutations: {
-       createPickupGame: async (parent, args) => {
+       createPickupGame: async (parent, args, { db }) => {
+           console.log(args);
             const pickupGame = {
-                start: args.input.start,
-                end: args.input.end,
-                location: await basketballFields.fieldById(args.input.basketballFieldId), 
-                host: await db.Player.findById(args.input.hostId)
+                start: args.input.start.value,
+                end: args.input.end.value,
+                basketballFieldId: args.input.basketballFieldId,
+                hostId: args.input.hostId
             }
             const results = await db.PickupGame.create(pickupGame);
             return results;
         },
-        removePickupGame: async (parent, args) => {
+        removePickupGame: async (parent, args, { db }) => {
             const gameToRemove = await db.PickupGame.findOneAndUpdate(args.id, {delete: true}, {new: true })
             return true;
         },
-        addPlayerToPickupGame: async (parent, args) => {
+        addPlayerToPickupGame: async (parent, args, { db }) => {
             const playerId = await db.Player.findById(args.input.playerId); 
             const result = await db.PickupGame.findOneAndUpdate(args.input.pickupGameId, {$push: {registerdPlayers: playerId}}, {new: true} )
             return result;
+        }
+     },
+     types: {
+        PickupGame: {
+            location: async (parent, args, { db }) => await db.PickupGame.findById(parent.basketballFieldId),
+            host: async (parent, args, { db }) => await db.Player.findById(parent.hostId),
         }
      }
     
