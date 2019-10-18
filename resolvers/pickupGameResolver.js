092d719eae5b610a61de52472f3f8cf5bb82e3c0
HjorturJ <hjorturjohann94@gmail.com>
2019-10-18 22:33:20 +0000

    un comment lol

--- resolvers/pickupGameResolver.js
+++ resolvers/pickupGameResolver.js
@@ -30,51 +30,41 @@ module.exports = {
             const startTimeMoment = moment(args.input.start.value);
             const endTimeMoment = moment(args.input.end.value);
             
-            //Check if the hostid points to an existing player as well
+            // A mutation which accepts an id as a field argument must check whether the resource with the provided id exists
             const hostPlayer = await db.Player.findById(pickupGame.hostId);
             if(hostPlayer == null) {
                 throw new errors.NotFoundError("HostId: Player with this id was not found!")
             }
 
-            //Check first if the basketballFieldId points to an existing thing
+            // A mutation which accepts an id as a field argument must check whether the resource with the provided id exists
             const field = await basketballFields.fieldById(pickupGame.basketballFieldId);
             if(field == null) {
                 throw new errors.NotFoundError("BasketballFieldId: Basketall field with this id was not found!");
             }
 
-            //If the basketballField exists, check if its closed, which it may not be to continue
+            // Pickup games cannot be added to a basketball field which has a status of closed
             if(field.status == "CLOSED") {
                 throw new errors.BasketballFieldClosedError("This basketball field has been marked closed!");
             }
 
-            //Check if game start date is before game end date (NOT ALLOWED)
+            // Pickup games that have an end date which comes before the start date cannot be created
             if(!startTimeMoment.isBefore(endTimeMoment)) {
                 throw new errors.UserInputError("Start time may not be before end time!");
             }
+            if(startTimeMoment.isBefore(moment())) {
+                throw new errors.UserInputError("You cannot schedule games in the past!");
+            }
             
-            //Check if difference between start and end date is less than 5 minutes or longer than 2 hours
+            // Pickup games can be at max 2 hours, but a minimum of 5 minutes
             const diffTime = endTimeMoment.diff(startTimeMoment, 'minutes');
             if(diffTime > 120 || diffTime < 5) {
                 throw new errors.UserInputError("Games can not be under 5 minutes or over 2 hours!");
             }
             
-            //Check if game may not start in the past (Date.now probably)
-            if(startTimeMoment.isBefore(moment())) {
-                throw new errors.UserInputError("You cannot schedule games in the past!");
-            }
-
-            //Check if there is another pickupGame at the same time on the same field (Overlap)
+            // Pickup games cannot overlap if they are being played in the same basketball field
             const allUpcomingPickupGames = (await db.PickupGame.find({})).filter(g => g.deleted === false);
             allUpcomingPickupGames.forEach(game => {
-                //Are the games on the same field?
-                console.log("game");
-                console.log(game.basketballFieldId);
-                console.log(pickupGame.basketballFieldId);
-                
                 if(game.basketballFieldId == pickupGame.basketballFieldId) {
-                    console.log("SAME FIELD FOUND!");
-                    //console.log(game.location.id);
-                    //console.log(pickupGame.basketballFieldId);
                     if(!startTimeMoment.isAfter(moment(game.end)) || 
                        !endTimeMoment.isBefore(moment(game.start))) {
                             console.log("OVERLAP!!!");
@@ -82,14 +72,14 @@ module.exports = {
                     }
                 }
             });
-            /*
+
             const results = await db.PickupGame.create(pickupGame);
             
             //Add host as the first registered player and add this game to the hosts played games
             await db.PickupGame.findByIdAndUpdate(results.id, { $push: { registeredPlayers: args.input.hostId } }, {new: true} )
             await db.Player.findByIdAndUpdate(args.input.hostId, { $push: { playedGames: results.id } }, {new: true});
             
-            return results;*/
+            return results;
         },
         removePickupGame: async (parent, args, { db }) => {
             const gameToRemove = await db.PickupGame.findByIdAndUpdate(args.id, { deleted: true }, {new: true })
